9242f842d27d4e2608520a42d0f8910dbe87c834
Vatsal Gupta <97608916+vatsalgupta25@users.noreply.github.com>
2024-10-25 14:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return '<h1>Hello this is Vatsal here, trying to deploy this.</h1>'
+    return '<h1>Hello this is Vatsal here, trying to deploy this for simpletern.</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
